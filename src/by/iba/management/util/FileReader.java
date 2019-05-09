697e5fd7415a0f0846fd3a599c32136dd9fd65d9
Anastasiya Tuharava <tuharava@gmail.com>
2019-05-09 19:47:31 +0300

    Updated

---
+++ src/by/iba/management/util/FileReader.java
@@ -0,0 +1,39 @@
+package by.iba.management.util;
+
+import by.iba.management.model.exception.ReadFileIOException;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FileReader {
+    private static final Logger logger = LogManager.getRootLogger();
+    public static ArrayList<String> readFile(String filePath) throws IOException {
+        ArrayList<String> projectsList = new ArrayList<>();
+        File file = new File(filePath);
+        try (FileInputStream in = new FileInputStream(file)) {
+            XSSFWorkbook workbook = new XSSFWorkbook(in);
+            XSSFSheet mySheet = workbook.getSheetAt(0);
+            for (Row row : mySheet) {
+                Iterator<Cell> cellIterator = row.cellIterator();
+                String str = "";
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    str = str.concat(cell.getStringCellValue()).concat(" ");
+                }
+                projectsList.add(str);
+            }
+        } catch (ReadFileIOException e) {
+            logger.error("File error or IO error: ", e);
+        }
+        return projectsList;
+    }
+}
