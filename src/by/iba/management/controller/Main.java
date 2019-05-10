070fe31bc2df416da845f62c2ced25d3d46741d2
Anastasiya Tuharava <tuharava@gmail.com>
2019-05-10 18:37:22 +0300

    Updated

--- src/by/iba/management/controller/Main.java
+++ src/by/iba/management/controller/Main.java
@@ -1,8 +1,11 @@
 package by.iba.management.controller;
 
-import by.iba.management.model.entity.Project;
-import by.iba.management.model.entity.ProjectsRepository;
+import by.iba.management.model.entity.*;
+import by.iba.management.util.DataWriterEmployee;
 import by.iba.management.util.DataWriterProject;
+import by.iba.management.util.EmployeeIdGenerator;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +15,14 @@ public class Main {
             System.out.println(p);
         }
         DataWriterProject.writeProjectToFile(ProjectsRepository.getProjectList());
+        ArrayList<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee(EmployeeIdGenerator.getEmployeeId(), "Bob", "Marley",
+                7, true, Position.JUNIOR_QA, EnglishLanguageLevel.C1,
+                new ProgrammingLanguage(true, false, false, false, false),
+                new Skills(true, true, true, true, false),
+                new Testing(false, false,false, false),
+                new Tools(true, true, false, true)));
+        DataWriterEmployee.writeEmployeeToFile(employeeList, p1);
+
     }
 }
