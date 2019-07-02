c92a12cf24bdb68c72c59481236a475c3e30d771
valeryanka <v.cat.mailbox@gmail.com>
2019-07-02 21:06:05 +0300

    update

---
+++ src/by/iba/management/view/fxml/ProjectsListController.java
@@ -0,0 +1,33 @@
+package by.iba.management.view.fxml;
+
+import by.iba.management.model.logic.impl.FindProjectImpl;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class ProjectsListController {
+
+    @FXML
+    Button fxFindProjectButton;
+    RadioButton fxSearchByProjectId;
+    RadioButton fxSearchByProjectName;
+    TextField fxFindProjectTextField;
+
+    @FXML
+    private void findProject(ActionEvent event){
+        String searchField = fxFindProjectTextField.getText();
+        Boolean id = fxSearchByProjectId.isSelected();
+        Boolean name = fxSearchByProjectName.isSelected();
+
+        if (id){
+            FindProjectImpl byId = new FindProjectImpl();
+            byId.findProjectById(Long.parseLong(searchField));
+            }
+        else if (name){
+            FindProjectImpl byName = new FindProjectImpl();
+            byName.findProjectByName(searchField);
+        }
+    }
+}
