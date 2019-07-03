94ef48c63350a88f480c856fdafa02d6a5a2a5fc
valeryanka <v.cat.mailbox@gmail.com>
2019-07-03 17:44:33 +0300

    update

--- src/by/iba/management/view/fxml/ProjectsListController.java
+++ src/by/iba/management/view/fxml/ProjectsListController.java
@@ -3,9 +3,6 @@ package by.iba.management.view.fxml;
 import by.iba.management.model.entity.Project;
 import by.iba.management.model.entity.ProjectsRepository;
 import by.iba.management.model.logic.impl.FindProjectImpl;
-import by.iba.management.util.ProjectIdGenerator;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -29,14 +26,22 @@ public class ProjectsListController {
 
     @FXML
     private void displayProjectsList() {
-        ObservableList<Project> projectsList =
+        ArrayList<Project> projectsList = ProjectsRepository.getProjectList();
+        for (Project p: projectsList) {
+            fxProjectId.setCellValueFactory(new PropertyValueFactory<Project, String>(p.getProjectId());
+            fxProjectName.setCellValueFactory(new PropertyValueFactory<Project, String>(p.getProjectName()));
+            fxProjectDescription.setCellValueFactory(new PropertyValueFactory<Project, String>(p.getProjectDescription()));
+            projectsListTable.setItems((ObservableList) projectsList);
+        }
+
+        /*ObservableList<Project> projectsList =
                 FXCollections.observableArrayList(ProjectsRepository.getProjectList());
         for (Project p : projectsList) {
             fxProjectId.setCellValueFactory(new PropertyValueFactory<Project, String>(p.getProjectId());
             fxProjectName.setCellValueFactory(new PropertyValueFactory<Project, String>(p.getProjectName()));
             fxProjectDescription.setCellValueFactory(new PropertyValueFactory<Project, String>(p.getProjectDescription()));
             projectsListTable.setItems(projectsList);
-        }
+        }*/
     }
 
     @FXML
@@ -45,9 +50,8 @@ public class ProjectsListController {
         ListView list = new ListView();
 
         list.setMaxHeight(180);
-        ObservableList<Project> projectsList =
-                FXCollections.observableArrayList(ProjectsRepository.getProjectList());
-        projectsListTable.setItems(projectsList);
+        ArrayList<Project> projectsList = ProjectsRepository.getProjectList();
+        projectsListTable.setItems((ObservableList) projectsList);
 
         String searchField = fxFindProjectTextField.getText();
         Boolean id = fxSearchByProjectId.isSelected();
@@ -56,7 +60,7 @@ public class ProjectsListController {
         fxSearchByProjectId.setToggleGroup(tg);
         fxSearchByProjectName.setToggleGroup(tg);
 
-        fxFindProjectButton.setOnAction(event -> {
+        fxFindProjectButton.setOnAction(ActionEvent event) {
             RadioButton rb = (RadioButton)tg.getSelectedToggle();
             if (rb.equals(fxSearchByProjectId)){
                 FindProjectImpl searchReasultById = new FindProjectImpl();
@@ -66,43 +70,6 @@ public class ProjectsListController {
                 FindProjectImpl searchResultByName = new FindProjectImpl();
                 searchResultByName.findProjectByName(searchField);
             }
-        });
-
-
-    }
-    public void handleSearchByKey2(String oldVal, String newVal) {
-        ObservableList<Project> projectsList =
-                FXCollections.observableArrayList(ProjectsRepository.getProjectList());
-        // If the number of characters in the text box is less than last time
-        // it must be because the user pressed delete
-        if ( oldVal != null && (newVal.length() < oldVal.length()) ) {
-            // Restore the lists original set of entries
-            // and start from the beginning
-            projectsListTable.setItems(projectsList);
         }
-
-        // Break out all of the parts of the search text
-        // by splitting on white space
-        String[] parts = newVal.toUpperCase().split(" ");
-
-        // Filter out the entries that don't contain the entered text
-        ObservableList<String> subentries = FXCollections.observableArrayList();
-        for ( Object entry: projectsListTable.getItems() ) {
-            boolean match = true;
-            String entryText = (String)entry;
-            for ( String part: parts ) {
-                // The entry needs to contain all portions of the
-                // search string *but* in any order
-                if ( ! entryText.toUpperCase().contains(part) ) {
-                    match = false;
-                    break;
-                }
-            }
-
-            if ( match ) {
-                subentries.add(entryText);
-            }
-        }
-        projectsListTable.setItems(subentries);
     }
 }
