6125a5935033e5e63649e54bfe25606de8b02bd1
valeryanka <v.cat.mailbox@gmail.com>
2019-07-03 15:46:21 +0300

    update

--- src/by/iba/management/controller/Main.java
+++ src/by/iba/management/controller/Main.java
@@ -1,30 +1,55 @@
 package by.iba.management.controller;
 
+import by.iba.management.model.entity.Project;
+import by.iba.management.model.entity.ProjectsRepository;
+import by.iba.management.util.DataWriterProject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.StageStyle;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main extends Application {
 
         public static void main(String[] args) {
-        launch(args);
-    }
+
+            Project p1 = new Project("HelloWorld", "First line");
+            ProjectsRepository.getProjectList().add(p1);
+            for (Project p : ProjectsRepository.getProjectList()) {
+                System.out.println(p);
+            }
+            DataWriterProject.writeProjectToFile(ProjectsRepository.getProjectList());
+                    /*ArrayList<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee(EmployeeIdGenerator.getEmployeeId(), "Bob", "Marley",
+                7, true, Position.JUNIOR_QA, EnglishLanguageLevel.C1,
+                new ProgrammingLanguage(true, false, false, false, false),
+                new Skills(true, true, true, true, false),
+                new Testing(false, false,false, false),
+                new Tools(true, true, false, true)));
+        DataWriterEmployee.writeEmployeeToFile(employeeList, p1);*/
+
+            launch(args);
+        }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         Stage stage = new Stage();
-        String name = "/by/iba/management/view/fxml/ProjectsList.fxml";
-        Parent root = FXMLLoader.load(getClass().getResource(name));
-        stage.setTitle("Projects List");
-        stage.setScene(new Scene(root));
+        FXMLLoader loader = new FXMLLoader();
+        String fxmlDocPath = "/by/iba/management/view/fxml/ProjectsList.fxml";
+        //FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+        //VBox root = (VBox) loader.load(fxmlStream);
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlDocPath));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.sizeToScene();
         stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setTitle("Projects List");
         stage.show();
-    }
 
+    }
 }
--- src/by/iba/management/view/fxml/ProjectsListController.java
+++ src/by/iba/management/view/fxml/ProjectsListController.java
@@ -1,11 +1,18 @@
 package by.iba.management.view.fxml;
 
+import by.iba.management.model.entity.Project;
+import by.iba.management.model.entity.ProjectsRepository;
 import by.iba.management.model.logic.impl.FindProjectImpl;
-import javafx.event.ActionEvent;
+import by.iba.management.util.ProjectIdGenerator;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
 
 public class ProjectsListController {
 
@@ -14,20 +21,92 @@ public class ProjectsListController {
     RadioButton fxSearchByProjectId;
     RadioButton fxSearchByProjectName;
     TextField fxFindProjectTextField;
+    TableView projectsListTable;
+    TableColumn fxProjectId;
+    TableColumn fxProjectName;
+    TableColumn fxProjectDescription;
 
     @FXML
-    private void findProject(ActionEvent event){
+    private void displayProjectsList() {
+        ObservableList<Project> projectsList =
+                FXCollections.observableArrayList(ProjectsRepository.getProjectList());
+        fxProjectId.setCellValueFactory(new PropertyValueFactory<Project, String>("projectId"));
+        fxProjectName.setCellValueFactory(new PropertyValueFactory<Project, String>("projectName"));
+        fxProjectDescription.setCellValueFactory(new PropertyValueFactory<Project, String>("projectDescription"));
+        projectsListTable.setItems(projectsList);
+    }
+
+    @FXML
+    private void findProject() {
+        fxFindProjectTextField.setPromptText("Search");
+        ListView list = new ListView();
+        fxFindProjectTextField.textProperty().addListener(
+                new ChangeListener() {
+                    public void changed(ObservableValue observable,
+                                        Object oldVal, Object newVal) {
+                        handleSearchByKey2((String)oldVal, (String)newVal);
+                    }
+                });
+
+        list.setMaxHeight(180);
+        ObservableList<Project> projectsList =
+                FXCollections.observableArrayList(ProjectsRepository.getProjectList());
+        projectsListTable.setItems(projectsList);
+
         String searchField = fxFindProjectTextField.getText();
         Boolean id = fxSearchByProjectId.isSelected();
         Boolean name = fxSearchByProjectName.isSelected();
+        ToggleGroup tg = new ToggleGroup();
+        fxSearchByProjectId.setToggleGroup(tg);
+        fxSearchByProjectName.setToggleGroup(tg);
 
-        if (id){
-            FindProjectImpl byId = new FindProjectImpl();
-            byId.findProjectById(Long.parseLong(searchField));
+        fxFindProjectButton.setOnAction(event -> {
+            RadioButton rb = (RadioButton)tg.getSelectedToggle();
+            if (rb.equals(fxSearchByProjectId)){
+                FindProjectImpl searchReasultById = new FindProjectImpl();
+                searchReasultById.findProjectById(Long.parseLong(searchField));
             }
-        else if (name){
-            FindProjectImpl byName = new FindProjectImpl();
-            byName.findProjectByName(searchField);
+            if (rb.equals(fxSearchByProjectName)){
+                FindProjectImpl searchResultByName = new FindProjectImpl();
+                searchResultByName.findProjectByName(searchField);
+            }
+        });
+
+
+    }
+    public void handleSearchByKey2(String oldVal, String newVal) {
+        ObservableList<Project> projectsList =
+                FXCollections.observableArrayList(ProjectsRepository.getProjectList());
+        // If the number of characters in the text box is less than last time
+        // it must be because the user pressed delete
+        if ( oldVal != null && (newVal.length() < oldVal.length()) ) {
+            // Restore the lists original set of entries
+            // and start from the beginning
+            projectsListTable.setItems(projectsList);
         }
+
+        // Break out all of the parts of the search text
+        // by splitting on white space
+        String[] parts = newVal.toUpperCase().split(" ");
+
+        // Filter out the entries that don't contain the entered text
+        ObservableList<String> subentries = FXCollections.observableArrayList();
+        for ( Object entry: projectsListTable.getItems() ) {
+            boolean match = true;
+            String entryText = (String)entry;
+            for ( String part: parts ) {
+                // The entry needs to contain all portions of the
+                // search string *but* in any order
+                if ( ! entryText.toUpperCase().contains(part) ) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if ( match ) {
+                subentries.add(entryText);
+            }
+        }
+        projectsListTable.setItems(subentries);
     }
 }
