38c1032749581550d86e35a372476a24b32a5161
Anastasiya Tuharava <tuharava@gmail.com>
2019-07-06 08:54:03 +0300

    Update

--- src/by/iba/management/controller/Main.java
+++ src/by/iba/management/controller/Main.java
@@ -12,32 +12,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application /*implements Runnable*/{
+public class Main extends Application{
 
         public static void main(String[] args) {
-
-                /*Project p1 = new Project("HelloWorld", "First line");
-                ProjectsRepository.getProjectList().add(p1);
-                for (Project p : ProjectsRepository.getProjectList()) {
-                        System.out.println(p);
-                }
-                DataWriterProject.writeProjectToFile(ProjectsRepository.getProjectList());*/
-
-                    /*ArrayList<Employee> employeeList = new ArrayList<>();
-        employeeList.add(new Employee(EmployeeIdGenerator.getEmployeeId(), "Bob", "Marley",
-                7, true, Position.JUNIOR_QA, EnglishLanguageLevel.C1,
-                new ProgrammingLanguage(true, false, false, false, false),
-                new Skills(true, true, true, true, false),
-                new Testing(false, false,false, false),
-                new Tools(true, true, false, true)));
-        DataWriterEmployee.writeEmployeeToFile(employeeList, p1);*/
-
                 launch(args);
         }
-        /*@Override
-        public void run () {
 
-        }*/
         @Override
         public void start(Stage primaryStage) throws IOException {
                 Stage stage = new Stage();
